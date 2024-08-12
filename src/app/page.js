32709e47a9dec059d9dd2f5c22f6071cae830f8c
Azain0009 <93192047+Azain0009@users.noreply.github.com>
2024-08-12 08:17:09 +0400

    init

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,225 @@
-import Image from "next/image";
+'use client';
+import { useEffect, useState } from "react";
+import { useInView } from 'react-intersection-observer';
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+import { SiAdobeaftereffects, SiAdobepremierepro, SiDavinciresolve, SiAdobephotoshop, SiAdobelightroom } from 'react-icons/si';
 
 export default function Home() {
+  const { ref: headerRef, inView: headerInView } = useInView({
+    triggerOnce: true,
+    threshold: 0.1, // Adjust this based on when you want to trigger the animation
+  });
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // Animation duration
+      once: true, // Whether animation should happen only once
+    });
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
+    <>
+      <style jsx global>{`
+        body {
+          font-family: 'Helvetica', 'Arial', sans-serif;
+        }
+        @keyframes slideToCenter {
+          from {
+            transform: translateX(-100%);
+            opacity: 0;
+          }
+          to {
+            transform: translateX(0);
+            opacity: 1;
+          }
+        }
+        .scroll-slide {
+          transform: translateX(-100%);
+          opacity: 0;
+          transition: transform 1s ease-out, opacity 1s ease-out;
+        }
+        .scroll-slide.show {
+          transform: translateX(0);
+          opacity: 1;
+        }
+      `}</style>
+      <div className="bg-[#020410] text-white">
+        <header className="py-32 h-screen flex items-center px-4 relative" data-aos="fade-up">
+          <div className="absolute inset-0 flex justify-center items-center -z-10">
+            <div className="absolute left-1/4 top-1/2 transform -translate-x-1/2 -translate-y-1/2 w-80 h-80 bg-gradient-radial"></div>
+          </div>
+          <div className="w-full max-w-screen-xl mx-auto text-center relative">
+            <div className="w-full px-2 md:text-left">
+              <h1 className={`text-3xl md:text-9xl text-white scroll-slide ${headerInView ? 'show' : ''}`} ref={headerRef}>
+                Azain Akil
+              </h1>
+              <p className={`text-3xl mb-8 md:text-3xl mt-4 text-white scroll-slide ${headerInView ? 'show' : ''}`}>
+                I craft bespoke video edits for individuals and brands
+              </p>
+              <a href="mailto:azainxc@gmail.com" className="mt-6 inline-block rounded-lg bg-white text-black p-3 transition-transform duration-300 ease-in-out transform hover:bg-black hover:text-white hover:scale-110" data-aos="fade-up" data-aos-delay="400">
+                Let's Collaborate
+              </a>
+            </div>
+          </div>
+        </header>
+
+        <section className="py-32" data-aos="fade-up">
+          <div className="container mx-auto px-4 text-center">
+            <h2 className="text-4xl font-semibold mb-8 text-white" data-aos="fade-up">My Creative Arsenal</h2>
+            <p className="text-gray-400 mb-8" data-aos="fade-up" data-aos-delay="100">
+              I use a variety of tools to get the edits just right, ensuring each project meets the highest standards of quality.
+            </p>
+            <div className="flex flex-wrap justify-center space-x-4">
+              <div className="flex-shrink-0 w-24 h-24 flex items-center justify-center" data-aos="fade-up" data-aos-delay="200">
+                <SiAdobepremierepro className="text-4xl sm:text-6xl" />
+              </div>
+              <div className="flex-shrink-0 w-24 h-24 flex items-center justify-center" data-aos="fade-up" data-aos-delay="300">
+                <SiAdobeaftereffects className="text-4xl sm:text-6xl" />
+              </div>
+              <div className="flex-shrink-0 w-24 h-24 flex items-center justify-center" data-aos="fade-up" data-aos-delay="400">
+                <SiDavinciresolve className="text-4xl sm:text-6xl" />
+              </div>
+              <div className="flex-shrink-0 w-24 h-24 flex items-center justify-center" data-aos="fade-up" data-aos-delay="500">
+                <SiAdobephotoshop className="text-4xl sm:text-6xl" />
+              </div>
+              <div className="flex-shrink-0 w-24 h-24 flex items-center justify-center" data-aos="fade-up" data-aos-delay="600">
+                <SiAdobelightroom className="text-4xl sm:text-6xl" />
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="py-32" data-aos="fade-up">
+          <div className="container mx-auto px-4">
+            <h2 className="text-4xl font-semibold mb-8 text-center">Work Visuals</h2>
+            <p className="text-lg text-gray-300 mb-8 w-1/2 mx-auto text-center py-4">
+              Explore some of my recent video projects that showcase my expertise in video editing. From cinematic storytelling to precise editing, each piece reflects my commitment to creating compelling and high-quality visual content. Whether it’s a brand promotion, a creative short film, or a personal project, I bring creativity and technical skill to every edit to ensure a captivating final product.
+            </p>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+              <div className="col-span-1 lg:col-span-2 row-span-2">
+                <iframe
+                  width="100%"
+                  height="100%"
+                  src="https://www.youtube.com/embed/8DFUTH7LWhM"
+                  title="Video 1"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="object-cover"
+                ></iframe>
+              </div>
+              <div className="col-span-1">
+                <iframe
+                  width="100%"
+                  height="100%"
+                  src="https://www.youtube.com/embed/4N6V3Z76CTc"
+                  title="Video 2"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="object-cover"
+                ></iframe>
+              </div>
+              <div className="col-span-1">
+                <iframe
+                  width="100%"
+                  height="100%"
+                  src="https://www.youtube.com/embed/n-jXmEi42qc"
+                  title="Video 3"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="object-cover"
+                ></iframe>
+              </div>
+              <div className="col-span-1 lg:col-span-2 row-span-2">
+                <iframe
+                  width="100%"
+                  height="100%"
+                  src="https://www.youtube.com/embed/q4gZ6JDn4-E"
+                  title="Video 4"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="object-cover"
+                ></iframe>
+              </div>
+              <div className="col-span-1">
+                <iframe
+                  width="100%"
+                  height="100%"
+                  src="https://www.youtube.com/embed/Ck9I3r2dFUY"
+                  title="Video 5"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="object-cover"
+                ></iframe>
+              </div>
+              <div className="col-span-1">
+                <iframe
+                  width="100%"
+                  height="100%"
+                  src="https://www.youtube.com/embed/BwPEU9BE644"
+                  title="Video 6"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                  className="object-cover"
+                ></iframe>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section className="py-32" data-aos="fade-up">
+          <div className="container mx-auto px-4 text-center">
+            <h2 className="text-4xl font-semibold mt-2 mb-8">My Philosophy</h2>
+            <p className="mt-4 w-full sm:w-1/2 mx-auto text-gray-300">
+              At the heart of my work lies a deep passion for storytelling through visuals. My approach combines creativity with technical expertise to produce engaging content that resonates with audiences. I believe in a collaborative process where client vision meets artistic innovation, resulting in unique and impactful video edits.
+            </p>
+          </div>
+        </section>
+
+        <section className="py-32" data-aos="fade-up">
+  <div className="container mx-auto px-4 text-center">
+    <h2 className="text-4xl font-semibold mb-8">Client Testimonials</h2>
+    <div className="relative flex flex-wrap justify-center gap-8 py-4">
+      <div className="w-full max-w-sm p-6 rounded-lg shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-105 testimonial-card" data-aos="fade-up" data-aos-delay="100">
+        <p className="text-gray-300 mb-4">
+          "Azain Akil's work is exceptional. His attention to detail and creative vision brought our project to life in ways we never imagined. Highly recommend!"
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+        <p className="font-semibold text-white">Nikhil Naik</p>
+        {/* <p className="text-gray-400">CEO, Tech Innovations</p> */}
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="w-full max-w-sm p-6 rounded-lg shadow-lg transition-transform duration-300 ease-in-out transform hover:scale-105 testimonial-card" data-aos="fade-up" data-aos-delay="200">
+        <p className="text-gray-300 mb-4">
+          "Working with Azain has been a game-changer for our brand. His video edits are not only high quality but also perfectly align with our vision and goals."
+        </p>
+        <p className="font-semibold text-white">Shreesh</p>
+        {/* <p className="text-gray-400">Creative Director, Artistry Co.</p> */}
       </div>
+    </div>
+  </div>
+</section>
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+        <section className="py-32" data-aos="fade-up">
+          <div className="container mx-auto px-4 text-center">
+            <h2 className="text-4xl font-semibold mt-2 mb-8">Interested in working together? Drop me a line.</h2>
+            <a href="mailto:azainxc@gmail.com" className="text-lg text-vibrant-teal underline">azainxc@gmail.com</a>
+          </div>
+        </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <footer className="bg-gray-700 py-4" data-aos="fade-up">
+          <div className="container mx-auto text-center">
+            <p className="text-gray-500">&copy; 2024 Azain Akil. All rights reserved.</p>
+          </div>
+        </footer>
       </div>
-    </main>
+    </>
   );
 }
